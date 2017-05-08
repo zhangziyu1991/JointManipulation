c41a2fbede8006298f690fa965f9191ba3de54c4
Mellina05 <meidipeng2018@u.northwestern.edu>
2017-05-08 16:12:51 -0500

    Added two walls and collision break

---
+++ Assets/Scripts/GroundCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCollision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("AAAAAA");
+        Debug.Log(col.gameObject);
+        if (col.gameObject.tag == "WallDown")
+        {
+            Debug.Log("BBBBBB");
+            Debug.Break();
+            //EndGame = true;​
+            //Destroy(col.gameObject);
+        }
+    }
+}
