a0af2aaf26508369ecadbe19458fdad600cbd828
Ziyu Zhang <zhangziyuchina@gmail.com>
2017-05-08 00:15:24 -0500

    new dummy control

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -9,7 +9,8 @@ public class Rotate : MonoBehaviour {
 	private Vector3 _mouseOffset;
 	//	private Vector3 _rotation;
 	private float _rotation;
-	private bool _isRotating;
+	private bool _isRotating=false;
+	public float speed;
 
 	void Start () {
 		_sensitivity = 0.4f;
@@ -18,19 +19,56 @@ public class Rotate : MonoBehaviour {
 	}
 
 	void Update() {
+		
 		if(_isRotating) {
 			// offset
-			_mouseOffset = (Input.mousePosition - _mouseReference);
+//			_mouseOffset = (Input.mousePosition - _mouseReference);
 
 			// apply rotation
 			//			_mouseOffset.x = Mathf.Clamp(-_mouseOffset.x * sensitivity, -Mathf.PI / 2, Mathf.PI/2) / (-sensitivity);
-			_rotation = -_mouseOffset.x * _sensitivity;
+			//			_rotation = -_mouseOffset.x * _sensitivity;
+
+//			Vector3 current = new Vector3 (transform.position.x - transform.parent.position.x, transform.position.y - transform.parent.position.y);
+
+			Vector3 p = new Vector3();
+			Camera  c = Camera.main;
+			Vector2 mousePos = new Vector2();
+
+			mousePos.x = Input.mousePosition.x;
+			mousePos.y = Input.mousePosition.y;
+
+			p = c.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 50));
+
+
+//			Vector3 target = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x , Input.mousePosition.y, 0));
+
+			Vector3	target2	= p - transform.parent.position;
+
+			Debug.Log (p);
+
+
+
+
+//			float step = speed * Time.deltaTime;
+
+//			Vector3 newDir = Vector3.RotateTowards(new Vector3(0, 0, -1), target - current, step, 0.0F);
+
+//			Vector2 rotation = new Vector2(Input.mousePosition.x, Input.mousePosition.y)  - new Vector2 (transform.parent.position.x, transform.parent.position.y);
 
 			// rotate
-			transform.parent.RotateAround(transform.parent.position, new Vector3(0, 0, 1), _rotation);
+			//			transform.parent.RotateAround(transform.parent.position, new Vector3(0, 0, -1), _rotation);
 
-			// store mouse
-			_mouseReference = Input.mousePosition;
+//			transform.parent.rotation = Quaternion.LookRotation (_rotation);
+
+
+//			transform.rotation = Quaternion.LookRotation (newDir);
+
+			transform.parent.LookAt (p, new Vector3(0, 0, -1));
+
+				// store mouse
+//				_mouseReference = Input.mousePosition;
+
+
 		}
 	}
 
@@ -39,7 +77,7 @@ public class Rotate : MonoBehaviour {
 		_isRotating = true;
 
 		// store mouse
-		_mouseReference = Input.mousePosition;
+//		_mouseReference = Input.mousePosition;
 	}
 
 	void OnMouseUp() {
