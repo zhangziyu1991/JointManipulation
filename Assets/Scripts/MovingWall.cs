7cf25132d6aaf1a4bedb2f759b714ab2a45adab8
Ziyu Zhang <zhangziyuchina@gmail.com>
2017-05-22 14:06:52 -0400

    removed redundant file

--- Assets/Scripts/MovingWall.cs
+++ Assets/Scripts/MovingWall.cs
@@ -2,32 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingWall : MonoBehaviour
-{
-
+public class MovingWall : MonoBehaviour {
+	public float speed;
+	public Transform start;
     public Transform target;
-    public float speed;
-    public Transform start;
-
+    
     private Vector3 frometh;
     private Vector3 untoeth;
 
-    void Start()
-    {
+    void Start() {
         frometh = start.position;
         untoeth = target.position;
     }
 
+//    void Update() {
+//        float step = speed * Time.deltaTime;
+//        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+//        if (transform.position.z < untoeth.z) {
+//            transform.position = frometh;
+//        }
+//    }
 
-    void Update()
-    {
-        float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
-        if (transform.position == untoeth)
-        {
-            //Debug.Log("AAAAA");
-            transform.position = frometh;
-        }
-    }
-
+	void Update() {
+		float step = speed * Time.deltaTime;
+		transform.position =  new Vector3(transform.position.x, transform.position.y, transform.position.z - step);
+		if (transform.position.z < untoeth.z) {
+			transform.position = new Vector3(transform.position.x, transform.position.y, frometh.z);
+		}
+	}
 }
