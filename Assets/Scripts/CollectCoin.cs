02cf9bac2e516a437ba126b0aa073a13460bba46
Ziyu Zhang <zhangziyuchina@gmail.com>
2017-05-24 15:36:43 -0500

    ,

--- Assets/Scripts/CollectCoin.cs
+++ Assets/Scripts/CollectCoin.cs
@@ -12,10 +12,15 @@ public class CollectCoin : MonoBehaviour {
     void OnCollisionEnter(Collision col) {
 		string[] tokens = scoreText.text.Split('：');
 		Int32.TryParse(tokens[1], out _currScore);	
+		Debug.Log ("curr score: " + tokens [1]);
+		Debug.Log ("Colliding into " + col.gameObject);
 
-        if (col.gameObject.tag == "coin") {
-            scoreText.text = "SCORE： " + (_currScore + 5);
-            Destroy(col.gameObject);
-        }
+		if (col.gameObject.tag == "Wall") {
+			Debug.Break ();
+			return;
+		} else if (col.gameObject.tag == "Coin") {
+			scoreText.text = "SCORE： " + (_currScore + 5);
+			Destroy (col.gameObject);
+		} 
     }
 }
