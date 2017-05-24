d268ccd2c280776bf50d4835a6008fc8842a973b
Ziyu Zhang <zhangziyuchina@gmail.com>
2017-05-24 10:47:48 -0500

    cleaned up

--- Assets/Scripts/Collide.cs
+++ Assets/Scripts/Collide.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Collide : MonoBehaviour {
-
+	
     void OnCollisionEnter(Collision col) {
+		Debug.Log (col.gameObject);
 		Debug.Break ();
     }
 }
