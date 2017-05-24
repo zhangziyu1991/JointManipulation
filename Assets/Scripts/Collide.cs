ee8a99650ce71b582631ef52c30e2e65d6085d53
Ziyu Zhang <zhangziyuchina@gmail.com>
2017-05-24 16:04:34 -0500

    cleaned up

--- Assets/Scripts/Collide.cs
+++ Assets/Scripts/Collide.cs
@@ -17,9 +17,6 @@ public class Collide : MonoBehaviour {
 		} else if (col.gameObject.tag == "Coin") {
 			string[] tokens = scoreText.text.Split('：');
 			Int32.TryParse(tokens[1], out _currScore);	
-			Debug.Log ("curr score: " + tokens [1]);
-			Debug.Log ("Colliding into " + col.gameObject);
-
 			scoreText.text = "SCORE： " + (_currScore + 5);
 			Destroy (col.gameObject);
 		} 
