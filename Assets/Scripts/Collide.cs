b532d5760c57192dc18120de33fcf1f0570058b9
Ziyu Zhang <zhangziyuchina@gmail.com>
2017-05-25 15:38:33 -0500

    m

--- Assets/Scripts/Collide.cs
+++ Assets/Scripts/Collide.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Collide : MonoBehaviour {
-	
-    public Text scoreText;
+
+	public Text scoreText;
 	private int _currScore;
-    
-    void OnCollisionEnter(Collision col) {
+
+	void OnCollisionEnter(Collision col) {
 		
-		if (col.gameObject.tag == "Wall") {
+		// Debug.Log ("I'm colliding in to " + col.gameObject + " of tag " col.gameObject.tag);
+
+		if (col.gameObject.tag == "Wall Component") {
 			Debug.Break ();
-			return;
+		
 		} else if (col.gameObject.tag == "Coin") {
-			string[] tokens = scoreText.text.Split('：');
-			Int32.TryParse(tokens[1], out _currScore);	
+			string[] tokens = scoreText.text.Split ('：');
+			Int32.TryParse (tokens [1], out _currScore);	
 			scoreText.text = "SCORE： " + (_currScore + 5);
-			Destroy (col.gameObject);
-		} 
-    }
+		
+		} else if (col.gameObject.tag == "Golden Coin") {
+			GameObject[] walls = GameObject.FindGameObjectsWithTag("Wall Wrapper");
+			Debug.Log ("This many walls: " + walls.Length);
+			foreach (GameObject wall in walls) {
+				Debug.Log ("lalalalal" + wall);
+				wall.GetComponent<Translate>().speed = 30;
+			}
+			walls = GameObject.FindGameObjectsWithTag("Wall");
+			foreach (GameObject wall in walls) {
+				foreach (Transform child in wall.transform) {
+					if (child.tag == "Wall Component") {
+						child.tag = "Wall Component Invincible Mode";
+					}
+				}
+			}
+		}
+	}
 }
--- Assets/Scripts/Translate.cs
+++ Assets/Scripts/Translate.cs
@@ -3,31 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Translate : MonoBehaviour {
+	
 	public float speed;
-	public Transform start;
-    public Transform target;
+	public Transform source;
+    public Transform sink;
+	public GameObject wall;
+
+	private GameObject _wall;
+
+	void Start() {
+		_wall = (GameObject) Instantiate(wall, wall.transform.position, Quaternion.identity);
+	}
     
-    private Vector3 frometh;
-    private Vector3 untoeth;
-
-    void Start() {
-        frometh = start.position;
-        untoeth = target.position;
-    }
-
-//    void Update() {
-//        float step = speed * Time.deltaTime;
-//        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
-//        if (transform.position.z < untoeth.z) {
-//            transform.position = frometh;
-//        }
-//    }
-
 	void Update() {
 		float step = speed * Time.deltaTime;
-		transform.position =  new Vector3(transform.position.x, transform.position.y, transform.position.z - step);
-		if (transform.position.z < untoeth.z) {
-			transform.position = new Vector3(transform.position.x, transform.position.y, frometh.z);
+		_wall.transform.position =  new Vector3(_wall.transform.position.x, _wall.transform.position.y, _wall.transform.position.z - step);
+		if (_wall.transform.position.z < sink.position.z) {
+			Destroy(_wall);
+			_wall = (GameObject) Instantiate(wall, source.position, Quaternion.identity);
+			if (speed == 30) {
+				foreach (Transform child in _wall.transform) {
+					if (child.tag == "Wall Component") {
+						child.tag = "Wall Component Invincible Mode";
+					}
+				}
+			}
 		}
 	}
 }
