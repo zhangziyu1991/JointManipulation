ab17e9b16b3940b1c3b2439990fa36cd2f9331e4
Ziyu Zhang <zhangziyuchina@gmail.com>
2017-05-30 16:12:07 -0700

    More Walls

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -3,23 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotate : MonoBehaviour {
-	private bool _isRotating=false;
 
-	void Update() {
-		if(_isRotating) {
-			Vector3 parentScreenPosition = Camera.main.WorldToScreenPoint(transform.parent.position);
-			Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-			Vector3 target = new Vector3 (screenPosition.x - parentScreenPosition.x,
-										  screenPosition.y - parentScreenPosition.y, 0);
-			transform.parent.rotation = Quaternion.LookRotation(new Vector3(0, 0, 1), target);
-		}
+	public float midRotation;
+	public float deltaRotation;
+
+	void OnMouseDrag() {
+
+		Vector3 parentScreenPosition = Camera.main.WorldToScreenPoint(transform.parent.position);
+		Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+		Vector3 target = new Vector3 (screenPosition.x - parentScreenPosition.x, screenPosition.y - parentScreenPosition.y, 0);
+		transform.parent.rotation = Quaternion.LookRotation(new Vector3(0, 0, 1), target);
+		//		if (WrapAngle(transform.parent.localEulerAngles.z - midRotation) > deltaRotation) {
+		//			transform.parent.transform.localRotation = Quaternion.Euler (transform.parent.transform.localEulerAngles.x,
+		//				transform.parent.transform.localEulerAngles.y,
+		//				UnwrapAngle(midRotation + deltaRotation));
+		//		} else if (WrapAngle(transform.parent.localEulerAngles.z - midRotation) < -deltaRotation) {
+		//			transform.parent.transform.localRotation = Quaternion.Euler (transform.parent.transform.localEulerAngles.x,
+		//				transform.parent.transform.localEulerAngles.y,
+		//				UnwrapAngle(midRotation - deltaRotation));
+		//		}
 	}
 
-	void OnMouseDown() {
-		_isRotating = true;
+	float WrapAngle(float angle) {
+		angle %= 360;
+		if (angle > 180)
+			return angle - 360;
+		return angle;
 	}
 
-	void OnMouseUp() {
-		_isRotating = false;
+	float UnwrapAngle (float angle){
+		if(angle >=0)
+			return angle;
+		angle = -angle%360;
+		return 360-angle;
 	}
 }
