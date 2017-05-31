7d01e539ffd397f74c50dd8728ad96a0d50a02bf
Ziyu Zhang <zhangziyuchina@gmail.com>
2017-05-30 18:00:00 -0700

    m

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -12,16 +12,33 @@ public class Rotate : MonoBehaviour {
 		Vector3 parentScreenPosition = Camera.main.WorldToScreenPoint(transform.parent.position);
 		Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 		Vector3 target = new Vector3 (screenPosition.x - parentScreenPosition.x, screenPosition.y - parentScreenPosition.y, 0);
-		transform.parent.rotation = Quaternion.LookRotation(new Vector3(0, 0, 1), target);
-		//		if (WrapAngle(transform.parent.localEulerAngles.z - midRotation) > deltaRotation) {
-		//			transform.parent.transform.localRotation = Quaternion.Euler (transform.parent.transform.localEulerAngles.x,
-		//				transform.parent.transform.localEulerAngles.y,
-		//				UnwrapAngle(midRotation + deltaRotation));
-		//		} else if (WrapAngle(transform.parent.localEulerAngles.z - midRotation) < -deltaRotation) {
-		//			transform.parent.transform.localRotation = Quaternion.Euler (transform.parent.transform.localEulerAngles.x,
-		//				transform.parent.transform.localEulerAngles.y,
-		//				UnwrapAngle(midRotation - deltaRotation));
-		//		}
+		Quaternion rotation = Quaternion.LookRotation(new Vector3(0, 0, 1), target);
+
+		var rotatedVector = Quaternion.Euler (0, 0, midRotation) * Vector3.up;	// local
+		var rotatedVector2 = transform.parent.localRotation * Vector3.up;		// local
+		float angle = Vector3.Angle(rotatedVector, rotatedVector2);
+
+		transform.parent.rotation = rotation;
+		if (angle > deltaRotation) {
+			transform.parent.localRotation = Quaternion.Euler (0, 0, midRotation + deltaRotation);
+		}
+
+		Debug.Log (rotatedVector + ", " + rotatedVector2 + ", " + angle);
+
+//		Vector3 fromVector = new Vector3(0, 0, midRotation)
+//		if (midRotation, )
+
+//		Debug.Log (transform.parent.localEulerAngles.z);
+
+//		if (WrapAngle(transform.parent.localEulerAngles.z - midRotation) > deltaRotation) {
+//			transform.parent.transform.localRotation = Quaternion.Euler (transform.parent.transform.localEulerAngles.x,
+//			transform.parent.transform.localEulerAngles.y,
+//			UnwrapAngle(midRotation + deltaRotation));
+//		} else if (WrapAngle(transform.parent.localEulerAngles.z - midRotation) < -deltaRotation) {
+//					transform.parent.transform.localRotation = Quaternion.Euler (transform.parent.transform.localEulerAngles.x,
+//						transform.parent.transform.localEulerAngles.y,
+//						UnwrapAngle(midRotation - deltaRotation));
+//				}
 	}
 
 	float WrapAngle(float angle) {
