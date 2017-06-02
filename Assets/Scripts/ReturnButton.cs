f61efced3ae4683e9004102c77d06f08ef37f6c5
Mellina05 <meidipeng2018@u.northwestern.edu>
2017-06-02 11:07:04 -0500

    help page

---
+++ Assets/Scripts/ReturnButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReturnButton : MonoBehaviour {
+
+	 public Button Return;
+
+    void Start()
+    {
+        Button btn = Return.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
